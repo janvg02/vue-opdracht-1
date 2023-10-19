5c7acf08c25b41748542a0007eb8b4e573b4b60e
janvg02 <jan.vangalen@student.pxl.be>
2023-10-19 21:22:11 +0200

    toegevoegd

---
+++ vue-project/src/App.vue
@@ -0,0 +1,20 @@
+<script>
+
+</script>
+
+
+<template>
+  <div>
+    <label for="product">Product</label>
+    <input type="text" id="product"/>
+    <button>Voeg dit product toe</button>
+  </div>
+  <ul>
+    <li></li>
+  </ul>
+</template>
+
+
+<style>
+
+</style>
